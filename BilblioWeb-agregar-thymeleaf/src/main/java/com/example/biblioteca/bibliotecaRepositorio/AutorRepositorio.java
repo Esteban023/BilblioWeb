6020d7811e4be6df1c8a80804c4b50902db982dd
Esteban023 <Esteban.Betancourt.Lozano@gmail.com>
2025-11-28 16:23:18 -0500

    se elimino la carpeta con los archivos repetidos

--- BilblioWeb-agregar-thymeleaf/src/main/java/com/example/biblioteca/bibliotecaRepositorio/AutorRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.example.biblioteca.bibliotecaRepositorio;
-
-import com.example.biblioteca.Model.Autor;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface AutorRepositorio extends JpaRepository<Autor, Integer> {}
